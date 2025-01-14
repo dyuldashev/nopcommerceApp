eb25dad8f2a173f5f2fec5e36a55dee11daf08e1
dyuldashev <madat.dilshodbek@gmail.com>
2025-01-14 19:36:35 +0100

    nopcommerce app UI tests are ready

---
+++ testCases/test_login.py
@@ -0,0 +1,51 @@
+import pytest
+from selenium import webdriver
+from pageObjects.LoginPage import LoginPage
+from utilities.readProperties import ReadConfig
+from utilities.customLogger import LogGen
+
+
+class Test_001_Login:
+    baseURL = ReadConfig.getApplicationURL()
+    useremail = ReadConfig.getUseremail()
+    password = ReadConfig.getPassword()
+
+    logger = LogGen.loggen()
+
+    @pytest.mark.regression
+    def test_homePageTitle(self, setup):
+        self.logger.info("*************** Test_001_Login *****************")
+        self.logger.info("****Started Home page title test ****")
+        self.driver = setup
+        self.driver.get(self.baseURL)
+        act_title = self.driver.title
+        if act_title == "Your store. Login":
+            assert True
+            self.driver.close()
+            self.logger.info("********** Home page title is passed **********")
+        else:
+            self.driver.save_screenshot(".\\Screenshots\\" + "test_homePageTitle.png")
+            self.driver.close()
+            self.logger.error("********** Home page title is failed **********")
+            assert False
+
+    @pytest.mark.sanity
+    @pytest.mark.regression
+    def test_login(self, setup):
+        self.logger.info("********** Verifying Login test **********")
+        self.driver = setup
+        self.driver.get(self.baseURL)
+        self.lp = LoginPage(self.driver)
+        self.lp.setUserName(self.useremail)
+        self.lp.setPassword(self.password)
+        self.lp.clickLogin()
+        act_title = self.driver.title
+        if act_title == "Dashboard / nopCommerce administration":
+            assert True
+            self.driver.close()
+            self.logger.info("********** Login test is passed **********")
+        else:
+            self.driver.save_screenshot(".\\Screenshots\\" + "test_login.png")
+            self.driver.close()
+            self.logger.error("********** Login test is failed **********")
+            assert False
